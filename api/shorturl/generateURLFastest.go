9a64174d8494aa3d0848d3a593e9deba4fd7d5c4
Syd Xu <prof.syd.xu@gmail.com>
2019-12-04 16:51:58 +0800

    add shorturl api

---
+++ api/shorturl/generateURLFastest.go
@@ -0,0 +1,66 @@
+package shorturl
+
+import (
+	"errors"
+
+	"github.com/XiBao/jos/api"
+	"github.com/XiBao/jos/sdk"
+	"github.com/XiBao/jos/sdk/request/shorturl"
+	"github.com/daviddengcn/ljson"
+)
+
+type GenerateURLFastestRequest struct {
+	api.BaseRequest
+	Domain      string `json:"domain"`      // 3.cn
+	Length      uint   `json:"length"`      // 短码长度，最小8位：100天，9位365天
+	RealUrl     string `json:"realUrl"`     // 长域名
+	ExpiredDays uint   `json:"expiredDays"` // 0为有访问自动续期，8位最大100天，9位最大365天
+}
+
+type GenerateURLFastestResponse struct {
+	ErrorResp *api.ErrorResponnse     `json:"error_response,omitempty"`
+	Data      *GenerateURLFastestData `json:"jingdong_shorturl_generateURLFastest_responce,omitempty"`
+}
+
+type GenerateURLFastestData struct {
+	Result *GenerateURLFastestResult `json:"generatejdurl_result,omitempty"`
+}
+
+type GenerateURLFastestResult struct {
+	Code     string `json:"code,omitempty"`
+	Message  string `json:"codeText,omitempty"`
+	ShortUrl string `json:"shortUrl,omitempty"`
+	RealUrl  string `json:"realUrl,omitempty"`
+	Username string `json:"username,omitempty"`
+	Ts       int64  `json:"ts,omitempty"`
+}
+
+// 生成短域新的api接口
+func GenerateURLFastest(req *GenerateURLFastestRequest) (*GenerateURLFastestResult, error) {
+	client := sdk.NewClient(req.AnApiKey.Key, req.AnApiKey.Secret)
+	client.Debug = req.Debug
+	r := shorturl.NewGenerateURLFastestRequest()
+	r.SetDomain(req.Domain)
+	r.SetLength(req.Length)
+	r.SetRealUrl(req.RealUrl)
+	r.SetExpiredDays(req.ExpiredDays)
+
+	result, err := client.Execute(r.Request, req.Session)
+	if err != nil {
+		return nil, err
+	}
+	var response GenerateURLFastestResponse
+	err = ljson.Unmarshal(result, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Data == nil {
+		return nil, errors.New("no data")
+	}
+
+	if response.Data.Result.Code != "200" {
+		return nil, &api.ErrorResponnse{Code: response.Data.Result.Code, ZhDesc: response.Data.Result.Message}
+	}
+	return response.Data.Result, nil
+}
